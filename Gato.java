c0563245a2f13d0b3df2cdcf9dc2bafb63678a75
DANIELJIMENEZH <159660355+DANIELJIMENEZH@users.noreply.github.com>
2024-06-02 08:46:39 -0600

    Add files via upload

---
+++ Gato.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.Gato;
+
+/**
+ *
+ * @author FER
+ */
+public class IMC {
+ public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
